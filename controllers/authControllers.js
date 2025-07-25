a95b3a2c411588309bf1623129240b37bf2bab24
Abdul-Kalam0 <abdulkalamblycomp@gmail.com>
2025-07-25 16:11:19 +0530

    feat(auth): add auth middleware and authentication routes

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -22,21 +22,62 @@ const registration = async (req, res) => {
       password: passHash,
     });
 
-    const token = await jwt.sign(
-      { id: newUser.email },
-      process.env.JWT_SECRET,
-      { expiresIn: "4h" }
-    );
-    res.cookie("token", token);
-
     res
       .status(201)
       .json({ message: "User Created Successfully!", user: newUser.name });
   } catch (error) {
     res
       .status(500)
-      .json({ message: "Internal server error ", error: error.message });
+      .json({ message: "Internal server error", error: error.message });
   }
 };
 
-module.exports = { registration };
+const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await UserModel.findOne({ email: email });
+    if (!user) return res.status(404).json({ message: "Invalid Credentials" });
+
+    const passwordMatch = await bcrypt.compare(password, user.passHash);
+    if (!passwordMatch)
+      return res.status(400).json({ message: "Invalid Credentials" });
+
+    const token = await jwt.sign(
+      { id: user._id, email: user.email },
+      process.env.JWT_SECRET,
+      { expiresIn: "id" }
+    );
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "Strict",
+      maxAge: 24 * 60 * 60 * 1000,
+    });
+
+    res.status(200).json({ message: "Login Successfully" });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Internal server error", error: error.message });
+  }
+};
+
+const logout = async (req, res) => {
+  try {
+    res.cookie("token", "", {
+      httpOnly: true,
+      secure: true,
+      sameSite: "Strict",
+      expires: new Date(0),
+    });
+    res.status(200).json({ message: "Logged out successfully" });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ messgae: "Internal server error", error: error.message });
+  }
+};
+
+module.exports = { registration, login, logout };
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const { registration } = require("../controllers/authControllers");
+const {
+  registration,
+  login,
+  logout,
+} = require("../controllers/authControllers");
+const authMiddleware = require("../middleware/authMiddleware");
 
-router.post("/register", registration);
+router.post("/register", authMiddleware, registration);
+router.post("/login", authMiddleware, login);
+router.post("/logout", authMiddleware, logout);
 
 module.exports = router;
